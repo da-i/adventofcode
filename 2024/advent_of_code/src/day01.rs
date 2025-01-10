2ac5404461bf9ddc75663e5950d6399f6be35acc
da-i <dami@rebergen.nl>
2025-01-10 22:33:15 +0100

    Move the error handling to main.

--- 2024/advent_of_code/src/day01.rs
+++ 2024/advent_of_code/src/day01.rs
@@ -2,13 +2,13 @@
 use std::fs::File;
 use std::io::{self, BufRead};
 
-fn read_file_to_lines(file_path: &str) -> impl Iterator<Item = io::Result<String>>{
+fn read_file_to_lines(file_path: &str) -> io::Result<impl Iterator<Item = io::Result<String>>>{
     // Check file and propegate error with ?
-    let file = File::open(file_path).unwrap();
+    let file = File::open(file_path)?;
     //  Buffer read object:
     let reader = io::BufReader::new(file);
     //  Return the lines iterator
-    reader.lines()
+    Ok(reader.lines())
 }
 
 pub fn solve() {
@@ -24,13 +24,17 @@ pub fn solve() {
     };
 
     println!("Using file: {}", file_path);
-    let lines = read_file_to_lines(file_path);
 
-    for line in lines {
-        match line {
-            Ok(content) => println!("{}", content),
-            Err(e) => eprintln!("Error reading line: {}", e),
+    match read_file_to_lines(file_path) {
+        Ok(lines) => {
+            for line in lines {
+                match line {
+                    Ok(content) => println!("{}", content),
+                    Err(e) => eprintln!("Error reading line: {}", e),
+                }
+            }
         }
+        Err(e) => eprintln!("Error opening file: {}", e),
     }
 }
 
